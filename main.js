1a2b66b4ee8ca64a7337bf0e39ccb16bc20c1304
inch6134 <148062152+inch6134@users.noreply.github.com>
2024-02-17 01:45:19 -0800

    Implement progress bar with mouse controls and keyboard support for player

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const video = document.querySelector("video");
 const rewindBtn = document.querySelector("#rewind");
 const fastForwardBtn = document.querySelector("#fast-forward");
 const volumeBtn = document.querySelector("#volume");
+const progressIndicator = document.querySelector("#progress-indicator");
+const progressBar = document.querySelector("#progress-bar");
+
 
 // functions
 
@@ -34,13 +37,48 @@ function updateVolumeIcon() {
   icon.textContent = video.muted ? "volume_off" : "volume_up";
 }
 
+function updateProgress() {
+  const progressPercentage = (video.currentTime / video.duration) * 100;
+
+  progressIndicator.style.width = `${progressPercentage}%`;
+}
+
+function seekingFn(e) {
+  const updatedTime = (e.offsetX / progressBar.offsetWidth) * video.duration;
+
+  video.currentTime = updatedTime;
+}
+
 // event listeners
 
 video.addEventListener("play", updatePlayNPauseIcon);
 video.addEventListener("click", playNpauseFn);
 video.addEventListener("pause", updatePlayNPauseIcon);
 playNpauseBtn.addEventListener("click", playNpauseFn);
+
 rewindBtn.addEventListener("click", () => rewindNforwardFn("rewind"));
 fastForwardBtn.addEventListener("click", () => rewindNforwardFn("forward"));
+
 video.addEventListener("volumechange", updateVolumeIcon);
 volumeBtn.addEventListener("click", muteUnmute);
+
+video.addEventListener("timeupdate", updateProgress);
+
+let mouseDown = false;
+
+progressBar.addEventListener("mousedown", () => {mouseDown = true;});
+progressBar.addEventListener("mouseup", () => {mouseDown = false});
+progressBar.addEventListener("click", seekingFn);
+progressBar.addEventListener("mousemove", (e) => mouseDown && seekingFn);
+
+window.addEventListener("keyup", (e) => {
+  if (e.code === "Space") {
+    playNpauseFn();
+  } else if (e.code === "ArrowLeft") {
+    rewindNforwardFn("rewind");
+  } else if (e.code === "ArrowRight") {
+    rewindNforwardFn("forward");
+  } else {
+    return;
+  }
+});
