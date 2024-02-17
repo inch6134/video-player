624f99eff827c46a660bd096194a71ba106d8a5e
inch6134 <148062152+inch6134@users.noreply.github.com>
2024-02-17 01:09:31 -0800

    Implement play/pause, rewind/forward, mute/unmute functionality

--- main.js
+++ main.js
@@ -1,12 +1,46 @@
 "use strict";
 
+// HTML references
 const playNpauseBtn = document.querySelector("#play-pause");
 const video = document.querySelector("video");
+const rewindBtn = document.querySelector("#rewind");
+const fastForwardBtn = document.querySelector("#fast-forward");
+const volumeBtn = document.querySelector("#volume");
+
+// functions
 
 function playNpauseFn() {
   video.paused ? video.play() : video.pause();
 }
 
 function updatePlayNPauseIcon() {
-  
+  const icon = playNpauseBtn.querySelector("i");
+  icon.textContent = "";
+
+  icon.textContent = video.paused ? "play_arrow" : "paused";
 }
+
+function rewindNforwardFn(type) {
+  video.currentTime += type === "rewind" ? -10 : 10;
+}
+
+function muteUnmute() {
+  video.muted = video.muted ? false : true;
+}
+
+function updateVolumeIcon() {
+  const icon = volumeBtn.querySelector("i");
+  icon.textContent = "";
+  icon.textContent = video.muted ? "volume_off" : "volume_up";
+}
+
+// event listeners
+
+video.addEventListener("play", updatePlayNPauseIcon);
+video.addEventListener("click", playNpauseFn);
+video.addEventListener("pause", updatePlayNPauseIcon);
+playNpauseBtn.addEventListener("click", playNpauseFn);
+rewindBtn.addEventListener("click", () => rewindNforwardFn("rewind"));
+fastForwardBtn.addEventListener("click", () => rewindNforwardFn("forward"));
+video.addEventListener("volumechange", updateVolumeIcon);
+volumeBtn.addEventListener("click", muteUnmute);
